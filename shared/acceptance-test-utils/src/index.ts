5ffa9f51fc10fbd10d5235951fa3dc3bd5559851
Tiago <tiagojacintodev@gmail.com>
2024-12-20 19:56:08 +0000

    feat: add acceptance test util for defining feature tests

---
+++ shared/acceptance-test-utils/src/index.ts
@@ -0,0 +1,20 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import { ScenariosDefinitionCallbackFunction } from 'jest-cucumber/dist/src/feature-definition-creation';
+
+export type Scope = 'frontend' | 'backend';
+
+export type ScopeBasedFeatureTagFilter = `@${Scope}`;
+
+export function defineFeatureTest(featureFilePath: string) {
+  return (
+    tagFilter: ScopeBasedFeatureTagFilter,
+    scenariosDefinitionCallback: ScenariosDefinitionCallbackFunction
+  ) =>
+    defineFeature(
+      loadFeature(featureFilePath, {
+        loadRelativePath: true,
+        tagFilter,
+      }),
+      scenariosDefinitionCallback
+    );
+}
