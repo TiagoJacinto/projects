b1701933b0d50398c80ab30376e34aaa4a8c8e85
Tiago <tiagojacintodev@gmail.com>
2024-12-23 14:37:54 +0000

    refactor(meal-delivery): move signup feature folder to the registration capacity folder

--- projects/meal-delivery/libs/users/registration/signup/acceptance-test/jest.config.ts
+++ projects/meal-delivery/libs/users/registration/signup/acceptance-test/jest.config.ts
@@ -1,11 +1,11 @@
 export default {
   displayName: 'meal-delivery-users-signup-acceptance-test',
-  preset: '../../../../../../jest.preset.js',
+  preset: '../../../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
-    '../../../../../../coverage/projects/meal-delivery/libs/users/signup/acceptance-test',
+    '../../../../../../../coverage/projects/meal-delivery/libs/users/registration/signup/acceptance-test',
 };
--- projects/meal-delivery/libs/users/registration/signup/contracts/jest.config.ts
+++ projects/meal-delivery/libs/users/registration/signup/contracts/jest.config.ts
@@ -1,11 +1,11 @@
 export default {
   displayName: 'meal-delivery-users-signup-contracts',
-  preset: '../../../../../../jest.preset.js',
+  preset: '../../../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
-    '../../../../../../coverage/projects/meal-delivery/libs/users/signup/contracts',
+    '../../../../../../../coverage/projects/meal-delivery/libs/users/registration/signup/contracts',
 };
