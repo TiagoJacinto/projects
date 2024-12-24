827e827bb1a9c58832f3cd1db08bcb4b5647dbfe
Tiago <tiagojacintodev@gmail.com>
2024-12-24 12:25:26 +0000

    chore: enable typescript eslint project service and add plugins and rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 const nx = require('@nx/eslint-plugin');
+const tseslint = require('typescript-eslint');
 
 const defaultDepConstraints = [
   {
@@ -23,16 +24,102 @@ const defaultDepConstraints = [
   },
 ];
 
+/** @type {import("eslint").Linter.Config} */
 module.exports = [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', 'node_modules', 'tmp', 'eslint.config.js'],
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(),
+      },
+    },
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      jest: require('eslint-plugin-jest'),
+      'import-helpers': require('eslint-plugin-import-helpers'),
+    },
     rules: {
+      ...require('eslint-config-prettier').rules,
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': [
+        'error',
+        {
+          ignorePrimitives: {
+            bigint: true,
+            number: true,
+            string: true,
+          },
+        },
+      ],
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      'jest/unbound-method': 'error',
+      '@typescript-eslint/no-explicit-any': [
+        'error',
+        {
+          ignoreRestArgs: true,
+        },
+      ],
+      '@typescript-eslint/no-empty-function': [
+        'warn',
+        {
+          allow: ['constructors'],
+        },
+      ],
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: {
+            attributes: false,
+            arguments: false,
+            returns: false,
+          },
+        },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          newlinesBetween: 'always',
+          groups: ['module', ['parent', 'sibling'], 'index'],
+          alphabetize: { order: 'asc', ignoreCase: true },
+        },
+      ],
       '@nx/enforce-module-boundaries': [
         'error',
         {
@@ -47,8 +134,6 @@ module.exports = [
           ],
         },
       ],
-      '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'off',
     },
   },
   {
