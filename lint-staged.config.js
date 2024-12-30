c53bcaddf249c3970c994a565992700af8c5f2e9
Tiago <tiagojacintodev@gmail.com>
2024-12-30 15:32:15 +0000

    Revert "chore: configure pre commit hook"
    This reverts commit 706db92408e4941a253da8a8b94a676ddc585295.

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '{projects,shared}/**/*.{ts,tsx}': (files) => {
-    return `nx affected --target=lint --files=${files.join(',')}`;
-  },
-  '{projects,shared}/**/*.{js,ts,jsx,tsx,json}': [
-    (files) => `nx affected --target=lint:fix --files=${files.join(',')}`,
-    (files) => `nx format:write --files=${files.join(',')}`,
-  ],
-};
