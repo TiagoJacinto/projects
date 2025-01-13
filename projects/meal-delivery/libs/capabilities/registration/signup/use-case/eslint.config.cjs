4485dcab67e464783b43c04dad2010238871f78a
Tiago <tiagojacintodev@gmail.com>
2025-01-13 16:08:30 +0000

    chore: remove eslint rule

--- projects/meal-delivery/libs/capabilities/registration/signup/use-case/eslint.config.cjs
+++ projects/meal-delivery/libs/capabilities/registration/signup/use-case/eslint.config.cjs
@@ -1,19 +1,3 @@
 const baseConfig = require('../../../../../../../eslint.config.cjs');
 
-module.exports = [
-  ...baseConfig,
-  {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
-        },
-      ],
-    },
-    languageOptions: {
-      parser: require('jsonc-eslint-parser'),
-    },
-  },
-];
+module.exports = [...baseConfig];
