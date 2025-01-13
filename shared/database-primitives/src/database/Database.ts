7875bfa26454e5b1d85a01feba5b1f1d33fe33d7
Tiago <tiagojacintodev@gmail.com>
2025-01-13 15:28:26 +0000

    feat: define base database class

--- shared/database-primitives/src/database/Database.ts
+++ shared/database-primitives/src/database/Database.ts
@@ -1,4 +1,31 @@
+import { type Maybe } from '@tiagojacinto/core-primitives';
+
 export interface Database<TConnection> {
   connect(): Promise<void>;
   getConnection(): TConnection;
+}
+
+export abstract class BaseDatabase<TConnection> implements Database<TConnection> {
+  protected abstract establishConnection(): Promise<void>;
+  protected abstract retrieveConnection(): Maybe<TConnection>;
+
+  async connect() {
+    const connection = this.retrieveConnection();
+
+    if (connection) {
+      throw new Error('Database already connected');
+    }
+
+    await this.establishConnection();
+  }
+
+  getConnection() {
+    const connection = this.retrieveConnection();
+
+    if (!connection) {
+      throw new Error('Database not connected');
+    }
+
+    return connection;
+  }
 }
