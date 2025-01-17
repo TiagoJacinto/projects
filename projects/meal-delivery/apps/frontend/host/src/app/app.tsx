3c46738ff9a6c8edbbdc8d36e7d36010604e5817
Tiago <tiagojacintodev@gmail.com>
2025-01-17 15:07:10 +0000

    chore(meal-delivery-frontend): initialize signup page module federation microfrontend

--- projects/meal-delivery/apps/frontend/host/src/app/app.tsx
+++ projects/meal-delivery/apps/frontend/host/src/app/app.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 import { Link, Route, Routes } from 'react-router-dom';
 
+const MealDeliverySignupPage = React.lazy(() => import('meal-delivery-signup-page/Module'));
+
 export function App() {
   return (
     <React.Suspense fallback={null}>
@@ -9,9 +11,13 @@ export function App() {
         <li>
           <Link to='/'>Home</Link>
         </li>
+        <li>
+          <Link to='/meal-delivery-signup-page'>MealDeliverySignupPage</Link>
+        </li>
       </ul>
       <Routes>
         <Route path='/' element={<NxWelcome title='meal-delivery-host' />} />
+        <Route path='/meal-delivery-signup-page' element={<MealDeliverySignupPage />} />
       </Routes>
     </React.Suspense>
   );
---
+++ projects/meal-delivery/apps/frontend/signup-page/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title='meal_delivery_signup_page' />
+    </div>
+  );
+}
+
+export default App;
